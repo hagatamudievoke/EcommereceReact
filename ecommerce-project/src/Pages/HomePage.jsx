0980a9b4715f2f43d8057f50bc715e61e840998d
hagatamudi <hagatamudi@evoketechnologies.com>
2025-12-17 23:07:51 +0530

    get quantity from backend

--- ecommerce-project/src/Pages/HomePage.jsx
+++ ecommerce-project/src/Pages/HomePage.jsx
@@ -1,19 +1,23 @@
-import axios from 'axios'
-import { useEffect,useState} from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
 import "./HomePage.css";
 
 export function HomePage() {
-    useEffect(()=>{
-        axios.get('http://localhost:3000/api/products')
-      .then((response)=>{
-          setProducts( response.data);})
-    },[]);
-    const [products,setProducts]=useState([]);
-  
+  const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+    axios.get("http://localhost:3000/api/cart-items").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header cart={cart} />
       <title>Ecommerce Project</title>
       <div className="home-page">
         <div className="products-grid">
@@ -22,25 +26,28 @@ export function HomePage() {
               <>
                 <div key={product.id} className="product-container">
                   <div className="product-image-container">
-                    <img
-                      className="product-image"
-                      src={product.image}
-                    />
+                    <img className="product-image" src={product.image} />
                   </div>
 
                   <div className="product-name limit-text-to-2-lines">
-                     {product.name}
+                    {product.name}
                   </div>
 
                   <div className="product-rating-container">
                     <img
                       className="product-rating-stars"
-                      src={`images/ratings/rating-${product.rating.stars*10}.png`}
+                      src={`images/ratings/rating-${
+                        product.rating.stars * 10
+                      }.png`}
                     />
-                    <div className="product-rating-count link-primary">{product.rating.count}</div>
+                    <div className="product-rating-count link-primary">
+                      {product.rating.count}
+                    </div>
                   </div>
 
-                  <div className="product-price">${(product.priceCents/100).toFixed(2)}</div>
+                  <div className="product-price">
+                    ${(product.priceCents / 100).toFixed(2)}
+                  </div>
 
                   <div className="product-quantity-container">
                     <select>
