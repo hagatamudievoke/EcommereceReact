5dec5ac0ed1b87de5532fff6f463924b42830465
hagatamudi <hagatamudi@evoketechnologies.com>
2025-12-17 19:11:51 +0530

    orders and tracking implementation

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,8 @@
 import {Route, Routes} from 'react-router'
 import { HomePage } from './Pages/HomePage';
 import { CheckOutPage } from './Pages/CheckOutPage';
+import { OrdersPage } from './Pages/OrdersPage';
+import { TrackingPage } from './Pages/TrackingPage';
 import './App.css'
 import './index.css'
 
@@ -10,6 +12,8 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="checkout" element={<CheckOutPage />}/>
+      <Route path="orders" element={<OrdersPage />}/>
+      <Route path="tracking" element={<TrackingPage />}/>
     </Routes>
     
   )
